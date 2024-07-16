c7b9a4472908a450ca5e754b8b8c7d76113a9898
zjhj0814 <zjhj0814@gmail.com>
2024-07-16 16:41:15 +0900

    2024.07.15

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,74 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.JoinFormula;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+//@TableGenerator(name = "MEMBER_SEQ_GENERATOR", table = "MY_SEQUENCES", pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@Table(name = "MBR")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    private Long id;
+    @Column(name = "name")
+    private String username;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+    @OneToMany
+    @JoinColumn(name = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+    //@ManyToMany
+    //@JoinTable(name = "MEMBER_PRODUCT")
+    //private List<Product> products = new ArrayList<>();
+    /*
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+    @Lob
+    private String description;
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+     */
+}
